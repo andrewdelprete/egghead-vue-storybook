33b3b26c6ff38266579c50b23d4640d1db9997c1
Andrew Del Prete <pathsofdesign@gmail.com>
2018-06-18 15:14:35 -0500

    Yo ho let's go

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,5 @@
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+module.exports = (baseConfig, env, defaultConfig) => {
+  defaultConfig.plugins.push(new VueLoaderPlugin());
+  return defaultConfig;
+};
